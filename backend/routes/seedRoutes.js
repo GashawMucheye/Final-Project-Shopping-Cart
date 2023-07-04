5148e3e760ed5d45de024edfeefb74f44db6c9e0
Gashaw Malede <gashaw2533@gmail.com>
2023-07-04 10:38:22 +0200

    generate sample users

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import Product from "../models/productModel.js";
+import User from "../models/userModels.js";
 import data from "../data.js";
 
 const seedRouter = Router();
@@ -9,7 +10,11 @@ seedRouter.get("/", async (req, res) => {
   await Product.deleteMany({});
   //! insert all products in data.js to product model in mongodb
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  //! remove  all previoues records in product model
+  await User.deleteMany({});
+  //! insert all users in data.js to product model in mongodb
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seedRouter;
