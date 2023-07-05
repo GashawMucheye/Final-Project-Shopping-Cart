0725a558a669d3721ca5e1f1535853365ac96833
Gashaw Malede <gashaw2533@gmail.com>
2023-07-06 01:49:26 +0200

    import tostify-container and tostify.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <Router>
       <div className="d-flex flex-column App">
+        <ToastContainer position="bottom-center" limit={1} />
         <Header />
         <Main />
         <Footer />
