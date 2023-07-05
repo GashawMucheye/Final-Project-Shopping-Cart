325536be566b2a04c756c768c754f6af4d578f39
Gashaw Malede <gashaw2533@gmail.com>
2023-07-05 03:29:31 +0200

    generate token by using jwt and creatd controllers folder that include product-controll, userControll

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-// import { data } from "./data.js";
 import { config } from "dotenv";
 import colors from "colors";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 colors;
 config();
@@ -12,8 +12,17 @@ import connectDB from "./config/db.js";
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
