1dcc2a599cd5996af0dc8971c9d32273784b9aea
Gashaw Malede <gashaw2533@gmail.com>
2023-07-06 01:54:23 +0200

    using usenavgate and use useState

--- frontend/src/screens/SignInScreen.jsx
+++ frontend/src/screens/SignInScreen.jsx
@@ -2,27 +2,63 @@ import { Helmet } from "react-helmet-async";
 
 import { HiOutlineMail } from "react-icons/hi";
 import { RiLockPasswordFill } from "react-icons/ri";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Container, Form, Button } from "react-bootstrap";
-
+import { Store } from "../contextApi/Store";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { getError } from "../utils";
 const SignInScreen = () => {
+  const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
   const redirect = redirectInUrl ? redirectInUrl : "/";
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/users/signin", {
+        email,
+        password,
+      });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate(redirect || "/");
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
   return (
     <Container className="small-container">
       <Helmet>
         <title>Sign In</title>
       </Helmet>
       <h1 className="my-3">Sign In</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3">
           <Form.Label>
             <HiOutlineMail />
             Email
           </Form.Label>
-          <Form.Control type="email" required placeholder="Enter Your Email" />
+          <Form.Control
+            type="email"
+            required
+            placeholder="Enter Your Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>
@@ -33,6 +69,9 @@ const SignInScreen = () => {
             type="password"
             required
             placeholder="Enter Your Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </Form.Group>
         <div className="mb-3">
