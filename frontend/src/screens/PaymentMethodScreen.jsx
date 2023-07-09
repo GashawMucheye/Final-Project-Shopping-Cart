eef3a09b5f068e7fd9a93f04c2b9f6fb62fd07ff
Gashaw Malede <gashaw2533@gmail.com>
2023-07-09 02:25:36 +0200

    created paymentMethodScreen component

---
+++ frontend/src/screens/PaymentMethodScreen.jsx
@@ -0,0 +1,64 @@
+import { Container, Form, Button } from "react-bootstrap";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Store } from "../contextApi/Store";
+import { Helmet } from "react-helmet-async";
+const PaymentMethodScreen = () => {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress, paymentMethod },
+  } = state;
+  const [paymentMethodName, setPaymentMethod] = useState(
+    paymentMethod || "payPal"
+  );
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    ctxDispatch({ type: "SAVE_PAYMENT_METHOD", payload: paymentMethodName });
+    localStorage.setItem("paymentMethod", paymentMethodName);
+    navigate("/placeorder");
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <Container className="small-container">
+        <Helmet>
+          <title>Payment Method</title>
+        </Helmet>
+        <h2 className="my-3">Payment Method</h2>
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="payPal"
+              label="PayPal"
+              value="PayPal"
+              checked={paymentMethodName === "payPal"}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="Stripe"
+              label="Stripe"
+              value="Stripe"
+              checked={paymentMethodName === "Stripe"}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <Button type="submit">continue</Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+export default PaymentMethodScreen;
