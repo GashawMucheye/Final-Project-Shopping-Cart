ef089354b7d54c0066fdab4ce18e931a53df2722
Gashaw Malede <gashaw2533@gmail.com>
2023-07-11 02:23:26 +0200

    created pagination component

---
+++ frontend/src/components/Pagination.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { GrLinkNext } from "react-icons/gr";
+import { GrLinkPrevious } from "react-icons/gr";
+const PaginationIt = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav className="d-flex container justify-content-center my-2">
+      <GrLinkPrevious className="mt-2" />
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item ">
+            <a
+              Link
+              to="!#"
+              onClick={() => paginate(number)}
+              className="page-link pagination-link"
+              style={{ cursor: "pointer" }}
+            >
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <GrLinkNext className="mt-2" />
+    </nav>
+  );
+};
+
+export default PaginationIt;
