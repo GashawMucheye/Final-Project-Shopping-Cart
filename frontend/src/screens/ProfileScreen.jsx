affed272aceaa1fa06cfef07d541fcacb4735f1d
Gashaw Malede <gashaw2533@gmail.com>
2023-07-18 14:32:55 +0200

    created profile screen

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,110 @@
+import { useContext, useReducer, useState } from "react";
+import { Store } from "../contextApi/Store";
+import { Helmet } from "react-helmet-async";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import { toast } from "react-toastify";
+import { getError } from "../utils";
+import axios from "axios";
+
+const actions = {
+  UPDATE_REQUEST: "UPDATE_REQUEST",
+  UPDATE_SUCCESS: "UPDATE_SUCCESS",
+  UPDATE_FAIL: "UPDATE_Fail",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.UPDATE_REQUEST:
+      return { ...state, loadingUpdate: true };
+
+    case actions.UPDATE_SUCCESS:
+      return { ...state, loadingUpdate: true };
+
+    case actions.UPDATE_FAIL:
+      return { ...state, loadingUpdate: true };
+
+    default:
+      return state;
+  }
+};
+
+const ProfileScreen = () => {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const [name, setName] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
+    loadingUpdate: false,
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/users/profile`,
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: actions.UPDATE_SUCCESS });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      toast.success("User updated successifully");
+    } catch (err) {
+      dispatch({ type: actions.UPDATE_FAIL });
+      toast.error(getError(err));
+    }
+  };
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>User Profile</title>
+      </Helmet>
+      <h1 className="my-3">User Profile</h1>
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button type="submit">Update</Button>
+      </form>
+    </Container>
+  );
+};
+
+export default ProfileScreen;
