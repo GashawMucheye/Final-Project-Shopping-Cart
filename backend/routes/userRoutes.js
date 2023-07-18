ea1b8304c7a6422885c8135cd31a1c0b9955d04f
Gashaw Malede <gashaw2533@gmail.com>
2023-07-18 14:30:01 +0200

    created profile api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
-import { getUserByEmail, creatingSignup } from "../controllers/userControll.js";
+import {
+  getUserByEmail,
+  creatingSignup,
+  updatingProfile,
+} from "../controllers/userControll.js";
+import { isAuth } from "../utils.js";
 
 const userRouter = Router();
 
 userRouter.post("/signin", getUserByEmail);
 userRouter.post("/signup", creatingSignup);
+userRouter.put("/profile", isAuth, updatingProfile);
 
 export default userRouter;
