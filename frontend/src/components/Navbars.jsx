72acc7d5925a98a4042738988355d6c0f17c19cc
Gashaw Malede <gashaw2533@gmail.com>
2023-07-18 12:05:02 +0200

    make responsive by using navbar-toggle&collapse

--- frontend/src/components/Navbars.jsx
+++ frontend/src/components/Navbars.jsx
@@ -15,47 +15,49 @@ const Navbars = ({ brandName }) => {
     localStorage.removeItem("paymentMethod");
   };
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container className="bg-info">
         <LinkContainer to="/">
           <Navbar.Brand>
             <strong> {brandName}</strong>
           </Navbar.Brand>
         </LinkContainer>
-        <Link to="/About">About</Link>
-
-        <Nav>
-          <Link to="/cart" className="nav-link">
-            <FaShoppingCart className="text-warning" />
-            {cart.cartItems.length > 0 && (
-              <Badge pill bg="danger">
-                {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-              </Badge>
-            )}
-          </Link>
-          {userInfo ? (
-            <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
-              <LinkContainer to="/profile">
-                <NavDropdown.Item>User Profile</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to="/orderhistory">
-                <NavDropdown.Item>Order History</NavDropdown.Item>
-              </LinkContainer>
-              <NavDropdown.Divider />
-              <Link
-                className="dropdown-item"
-                to="#Signout"
-                onClick={signoutHandler}
-              >
-                Signout
-              </Link>
-            </NavDropdown>
-          ) : (
-            <Link className="nav-link" to="/signin">
-              SignIn
+        {/* <Link to="/About">About</Link> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto w-100 justify-content-end">
+            <Link to="/cart" className="nav-link">
+              <FaShoppingCart className="text-warning" />
+              {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}
             </Link>
-          )}
-        </Nav>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>User Profile</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/orderhistory">
+                  <NavDropdown.Item>Order History</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Divider />
+                <Link
+                  className="dropdown-item"
+                  to="#Signout"
+                  onClick={signoutHandler}
+                >
+                  Signout
+                </Link>
+              </NavDropdown>
+            ) : (
+              <Link className="nav-link" to="/signin">
+                SignIn
+              </Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
