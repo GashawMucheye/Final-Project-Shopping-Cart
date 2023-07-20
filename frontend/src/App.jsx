b1037e6ad41b953d1438608fd0a1a0d6a15ba80e
Gashaw Malede <gashaw2533@gmail.com>
2023-07-20 02:20:44 +0200

    created variable sidebaris open by using usestate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,27 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import SideBar from "./components/SideBar";
 
 const App = () => {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
   return (
     <Router>
-      <div className="d-flex flex-column App">
+      <div
+        className={
+          sidebarIsOpen
+            ? "d-flex flex-column App active-cont"
+            : "d-flex flex-column App"
+        }
+      >
         <ToastContainer position="bottom-center" limit={1} />
-        <Header />
+        <Header setSidebarIsOpen={setSidebarIsOpen} />
+        <SideBar
+          sidebarIsOpen={sidebarIsOpen}
+          setSidebarIsOpen={setSidebarIsOpen}
+        />
         <Main />
         <Footer />
       </div>
