553cca88e7720ae8d834790c31e79871880d9b13
Gashaw Malede <gashaw2533@gmail.com>
2023-07-20 02:19:56 +0200

    import getcategoris that created in controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,13 @@ import {
   getProducts,
   getFindBySlug,
   GetFindById,
+  getCategories,
 } from "../controllers/productControll.js";
 
 const productRouter = Router();
 
 productRouter.get("/", getProducts);
+productRouter.get("/categories", getCategories);
 productRouter.get("/slug/:slug", getFindBySlug);
 productRouter.get("/:id", GetFindById);
 
