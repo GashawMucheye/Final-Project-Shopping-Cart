56cdb741117b33983be531ba50d24e88ae781d3a
Gashaw Malede <gashaw2533@gmail.com>
2023-07-21 12:37:26 +0200

    import get serch

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,13 @@ import {
   getFindBySlug,
   GetFindById,
   getCategories,
+  getSearch,
 } from "../controllers/productControll.js";
 
 const productRouter = Router();
 
 productRouter.get("/", getProducts);
+productRouter.get("/search", getSearch);
 productRouter.get("/categories", getCategories);
 productRouter.get("/slug/:slug", getFindBySlug);
 productRouter.get("/:id", GetFindById);
