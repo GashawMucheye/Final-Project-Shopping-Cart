e7f77e2a932dedd6a13563d10d00d44c96c36dde
gashaw mucheye <gashaw2533@gmail.com>
2023-07-31 20:18:19 +0200

    implement backend api for delete

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -107,6 +107,23 @@ orderRouter.get(
   })
 );
 
+//delete
+
+orderRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.deleteOne();
+      res.send({ message: 'Order Deleted' });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
 //deliver
 orderRouter.put(
   '/:id/deliver',
