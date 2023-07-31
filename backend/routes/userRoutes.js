b9be0f72d5f88abcfeeee57b47ccc54a6520ca80
gashaw mucheye <gashaw2533@gmail.com>
2023-07-31 21:25:28 +0200

    implement backend api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,22 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import {
   getUserByEmail,
   creatingSignup,
   updatingProfile,
-} from "../controllers/userControll.js";
-import { isAuth } from "../utils.js";
+  getUsers,
+  getUserById,
+  updateUserById,
+} from '../controllers/userControll.js';
+import { isAuth, isAdmin } from '../utils.js';
 
 const userRouter = Router();
+userRouter.get('/', isAuth, isAdmin, getUsers);
+userRouter.get('/:id', isAuth, isAdmin, getUserById);
+userRouter.put('/:id', isAuth, isAdmin, updateUserById);
 
-userRouter.post("/signin", getUserByEmail);
-userRouter.post("/signup", creatingSignup);
-userRouter.put("/profile", isAuth, updatingProfile);
+userRouter.post('/signin', getUserByEmail);
+userRouter.post('/signup', creatingSignup);
+userRouter.put('/profile', isAuth, updatingProfile);
 
 export default userRouter;
