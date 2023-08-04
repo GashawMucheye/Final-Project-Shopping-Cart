e674bc8bccf1c434d51c4279ff9d5a160f9dcaca
gashaw mucheye <gashaw2533@gmail.com>
2023-08-04 13:26:29 +0200

    create Email order receipt by melgun

--- README.md
+++ README.md
@@ -308,6 +308,7 @@ npm install mongoose
     - implement backen api for delete
 
 50. Choose Address On Google Map
+
     1. create google map credentials
     2. update .env file with Google Api Key
     3. create api to send google api to frontend
@@ -317,3 +318,11 @@ npm install mongoose
     7. install @react-google-maps/api
     8. use it in shipping screen
     9. apply map to the checkout screen
+
+51. Email order receipt by mailgun
+    1. create mailgun account
+    2. add and verify your domain to mailgun
+    3. install mailgun-js
+    4. set api key in env file
+    5. change pay order in orderRouter
+    6. send email order receipt
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,7 +6,7 @@ import Product from '../models/productModel.js';
 
 import { generateToken } from '../utils.js';
 
-import { isAuth, isAdmin } from '../utils.js';
+import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from '../utils.js';
 
 const orderRouter = express.Router();
 
@@ -145,7 +145,10 @@ orderRouter.put(
   '/:id/pay',
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'email name'
+    );
     if (order) {
       order.isPaid = true;
       order.paidAt = Date.now();
@@ -157,6 +160,23 @@ orderRouter.put(
       };
 
       const updatedOrder = await order.save();
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: 'gashaw <gashaw240985@outlook.com>',
+            to: `${order.user.name} <${order.user.email}>`,
+            subject: `New order ${order._id}`,
+            html: payOrderEmailTemplate(order),
+          },
+          (error, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+            }
+          }
+        );
       res.send({ message: 'Order Paid', order: updatedOrder });
     } else {
       res.status(404).send({ message: 'Order Not Found' });
