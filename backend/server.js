4092f1b85211501fa65ef4b399097a3fe90be2d6
gashaw mucheye <gashaw2533@gmail.com>
2023-08-29 02:15:15 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'))
 );
 const port = process.env.PORT || 5000;
 
